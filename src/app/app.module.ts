68c8431e991bccd76ebc056ffd55a334829a053c
Yohann <yohann@freshcore.fr>
2022-06-16 17:02:42 +0200

    install angular + component header, diaporama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DiaporamaComponent } from './diaporama/diaporama.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DiaporamaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/diaporama/diaporama.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diaporama',
+  templateUrl: './diaporama.component.html',
+  styleUrls: ['./diaporama.component.scss']
+})
+export class DiaporamaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
